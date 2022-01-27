9575ff1cf73fc0d044dee9b566f1c15b4badb438
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-01-27 12:15:55 +0600

    Update echo_bot.py

--- echo_bot.py
+++ echo_bot.py
@@ -1,19 +1,28 @@
-import os
-import config
-import telebot
-from flask import Flask
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from config import TOKEN
 
 
-token = "5093935580:AAGLpMwaKPBwXZx9I8RrgPQg2sNVWGgt228"
-APP_URL = f'https://echo-bot-418.herokuapp.com/'
-bot = telebot.TeleBot("5093935580:AAGLpMwaKPBwXZx9I8RrgPQg2sNVWGgt228")
-server = Flask(__name__)
+bot = Bot(token="5093935580:AAGLpMwaKPBwXZx9I8RrgPQg2sNVWGgt228")
+dp = Dispatcher(bot)
 
 
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли
-    bot.send_message(message.chat.id, message.text)
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+
 
 if __name__ == '__main__':
-     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    executor.start_polling(dp)
